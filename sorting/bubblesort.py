471de119350ffbf23bd1d1157275ecb03c979e39
coherent17 <78169365+coherent17@users.noreply.github.com>
2021-06-11 00:33:08 +0800

    bubblesort

---
+++ sorting/bubblesort.py
@@ -0,0 +1,22 @@
+data=[16,25,39,27,12,8,45,63]
+
+def bubbleSort(array):
+    for i in range(len(array)-1):
+        for j in range(0,len(array)-i-1):
+            if array[j]>array[j+1]:
+                #swap the element
+                array[j],array[j+1]=array[j+1],array[j]
+    return array
+
+def Modified_bubbleSort(array):
+    for i in range(len(array)-1):
+        flag=True
+        for j in range(0,len(array)-i-1):
+            if array[j]>array[j+1]:
+                #swap the element and let flag become false
+                array[j],array[j+1]=array[j+1],array[j]
+                flag=False
+
+                if flag==True:
+                    break
+    return array
